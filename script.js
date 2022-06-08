e0189edc6cb5979e29c57a88d92bb707f2e2c1af
Taynara <taynarafreitasdesousa@gmail.com>
2022-06-08 15:43:33 -0300

    Beginning program

---
+++ script.js
@@ -0,0 +1,12 @@
+var h = window.prompt('Type your height:')
+var peso = window.prompt('Type your weight:')
+var IMC = peso/(h**2)
+window.alert('IMC é:' + IMC)
+
+
+var h = window.prompt('Type your height:')
+var peso = window.prompt('Type your weight:')
+var IMC = peso/(h**2)
+window.alert('IMC é: ' + IMC)
+
+  
